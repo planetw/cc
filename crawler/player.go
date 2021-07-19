043afafaa5d7dae87f1985126e6b1186e9d08c04
planetw <planetw.git@gmail.com>
2021-07-20 00:00:20 +0800

    Add team struct

--- crawler/player.go
+++ crawler/player.go
@@ -7,6 +7,15 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+type player struct {
+	Name string
+}
+
+type team struct {
+	Name    string
+	Players []player
+}
+
 func (c *Crawler) ScrapePlayer() {
 	doc, err := c.Load("https://www.cpbl.com.tw/player")
 	if err != nil {
@@ -15,13 +24,26 @@ func (c *Crawler) ScrapePlayer() {
 
 	doc.
 		Find(".PlayersList dl").
-		Children().
 		Each(func(i int, s *goquery.Selection) {
-			switch node := goquery.NodeName(s); node {
-			case "dt":
-				fmt.Printf("Team Name: %s", s.Text())
-			case "dd":
-				fmt.Printf("Player Name: %s", s.Text())
-			}
+			t := &team{}
+
+			s.
+				Children().
+				Each(func(i int, s *goquery.Selection) {
+					switch node := goquery.NodeName(s); node {
+					case "dt":
+						t.Name = s.Text()
+						break
+					case "dd":
+						p := player{
+							Name: s.Text(),
+						}
+
+						t.Players = append(t.Players, p)
+						break
+					}
+				})
+
+			fmt.Printf("%v", t)
 		})
 }
